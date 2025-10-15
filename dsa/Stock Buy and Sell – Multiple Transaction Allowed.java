5d57fbc3729e1aebc6ae0cfc2b048fbf56b3e144
saisarathmadakam <160226471+saisarathmadakam@users.noreply.github.com>
2025-10-15 22:50:40 +0530

    Implement maximumProfit method for stock transactions

---
+++ dsa/Stock Buy and Sell – Multiple Transaction Allowed.java
@@ -0,0 +1,14 @@
+// User function Template for Java
+class Solution {
+    public int maximumProfit(int prices[]) {
+        int profit=0;
+         for(int i =1;i<prices.length;i++)
+         {
+             if(prices[i]>prices[i-1])
+             
+                 profit+=prices[i] - prices[i-1];
+             
+         }
+        return profit;
+    }
+}
